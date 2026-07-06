d09c6d7841a6d27db10cc6f085fb497cbb984e1c
Gigilok <cliquefeirago@gmail.com>
2026-07-06 13:33:06 -0300

    Update config.h

--- nRFBox/config.h
+++ nRFBox/config.h
@@ -54,10 +54,6 @@
 #include <SD.h>
 #include <Update.h>
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-
-Adafruit_NeoPixel pixels(1, 14, NEO_GRB + NEO_KHZ800);
-
 // BLE-specific dependencies
 #include <BLEDevice.h>
 #include <BLEUtils.h>
@@ -80,7 +76,9 @@ Adafruit_NeoPixel pixels(1, 14, NEO_GRB + NEO_KHZ800);
 #include <esp_bt.h>
 #include <esp_wifi.h>
 
-// External declarations
+// External declarations (Ajustadas para não gerar erro de redefinição)
+extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2;
+extern Adafruit_NeoPixel pixels;
 extern bool neoPixelActive;
 extern uint8_t oledBrightness;
 
