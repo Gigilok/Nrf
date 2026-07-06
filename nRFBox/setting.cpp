4c9fe89546aa5930fe0cc607cdb043271284026e
Gigilok <cliquefeirago@gmail.com>
2026-07-06 13:26:24 -0300

    Update setting.cpp

--- nRFBox/setting.cpp
+++ nRFBox/setting.cpp
@@ -6,10 +6,21 @@
 #include "setting.h"
 #include "icon.h"
 #include "config.h"
+#include <U8g2lib.h>          // Necessário para o objeto u8g2
+#include <Adafruit_NeoPixel.h> // Necessário para o objeto pixels
 
+// --- Rádios (Já estavam aqui) ---
 RF24 RadioA(NRF_CE_PIN_A, NRF_CSN_PIN_A);
 RF24 RadioB(NRF_CE_PIN_B, NRF_CSN_PIN_B);
 RF24 RadioC(NRF_CE_PIN_C, NRF_CSN_PIN_C);
+
+// --- Definição única das variáveis (SEM 'extern' aqui) ---
+// Note que o pino 14 é o que definimos para o NeoPixel
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+Adafruit_NeoPixel pixels(1, 14, NEO_GRB + NEO_KHZ800);
+
+bool neoPixelActive = false;
+uint8_t oledBrightness = 128;
 
 void setRadiosNeutralState() {
   RadioA.stopListening();
