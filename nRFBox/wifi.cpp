7e52d5ab6d1a3337e4ebc5c0056a42769c6cd22d
Gigilok <cliquefeirago@gmail.com>
2026-07-06 13:43:31 -0300

    Update wifi.cpp

--- nRFBox/wifi.cpp
+++ nRFBox/wifi.cpp
@@ -182,7 +182,6 @@ bool scanning = false;
 uint32_t last_packet_time = 0;
 String lastNeoPixelColour = "0"; 
 
-}
 
 void wsl_bypasser_send_raw_frame(const uint8_t *frame_buffer, int size) {
     esp_err_t res = esp_wifi_80211_tx(WIFI_IF_AP, frame_buffer, size, false);
