c0f157f688de877a14414d841855175b960325bc
Gigilok <cliquefeirago@gmail.com>
2026-07-06 13:36:46 -0300

    Update setting.h

--- nRFBox/setting.h
+++ nRFBox/setting.h
@@ -28,8 +28,8 @@ extern uint8_t oledBrightness;
 
 //extern bool neoPixelActive;
 
-bool neoPixelActive = false;
-uint8_t oledBrightness = 100;
+extern bool neoPixelActive;
+extern uint8_t oledBrightness;
 
 extern RF24 RadioA;
 extern RF24 RadioB;
